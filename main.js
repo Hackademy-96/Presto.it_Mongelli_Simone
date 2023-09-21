4c67ec8e40a27005b90f40ff8a06242b474a9afb
Simone Mongelli <mongelli.simone.2004@gmail.com>
2023-09-21 18:45:15 +0200

    da finire sezione cards

--- main.js
+++ main.js
@@ -7,3 +7,45 @@ window.addEventListener("scroll", ()=>{
         navbar.classList.remove("navbar-wrap")
     }
 })
+
+let articlesNumber = document.querySelector("#articlesNumber")
+let usersNumber = document.querySelector("#usersNumber")
+let productsNumber = document.querySelector("#productsNumber")
+
+
+function creatInterval(finalN, elemento, frequenza ){
+let counter = 0;
+
+let interval = setInterval( ()=>{
+    if(counter < finalN){
+    counter++
+    elemento.innerHTML = counter;
+    console.log(counter);
+}else {
+    clearInterval(interval)
+}
+},frequenza )
+}
+
+
+
+
+
+
+
+// INTERSECTION OBSERVER
+let isEntered= false;
+let observer = new IntersectionObserver( (entra)=>{
+entra.forEach( (entry)=>{
+    if(entry.isIntersecting && isEntered ==false){
+        creatInterval(1708,articlesNumber, 5)
+        creatInterval(453, usersNumber, 20)
+        creatInterval(845, productsNumber, 10)
+        isEntered = true;
+
+    }
+} )
+} )
+
+
+observer.observe(articlesNumber)
